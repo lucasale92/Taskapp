b15db0309c863e146b88dca90d161881bd1a28c1
Lucas <riveros.lucas07@gmail.com>
2024-03-19 01:24:03 -0300

    ultima prueba bd

--- django_api/settings.py
+++ django_api/settings.py
@@ -93,11 +93,12 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # }
  
 
-DATABASES = {'default' : dj_database_url.config(
-    default='postgres://...',
-    conn_max_age=600,
-    conn_health_checks=True,
-)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 # DATABASES = {
 #     "default":
